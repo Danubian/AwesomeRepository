cf4b83f4f6c832b3248eade800dde58a3ee72034
Zack Maldonado <stjohnzack@yahoo.com>
2015-06-28 02:23:58 -0400

    Adding database

---
+++ db_init.js
@@ -0,0 +1,39 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./awesome.db');
+
+db.serialize(function() {
+  console.log("Database Serialization Initializing...");
+
+  //Setting up info tables
+  setupTable("ships", "(id TEXT UNIQUE, red TEXT, green TEXT, blue TEXT, alpha TEXT)");
+
+
+  //testUsers();
+
+  console.log("Tables initialized!");
+});
+
+// function testUsers() {
+// 	var stmt = db.prepare("INSERT INTO users VALUES (?, ?, ?)");
+// 	for (var i = 0; i < 2; i++) {
+// 		stmt.run("device-" + i, "" + i*1, "" + i*2);
+// 	}
+// 	stmt.finalize();
+
+// 	db.each("SELECT id, x, y FROM users", function(err, row) {
+// 	  console.log(row.id + ": " + row.x + ", " + row.y);
+// 	});
+}
+
+function setupTable(table_name, table_columns_names) {
+	console.log("Setting up " + table_name + "...");
+
+	db.run("DROP TABLE IF EXISTS " + table_name);
+	db.run("CREATE TABLE " + table_name + " " + table_columns_names);
+
+	console.log("Table " + table_name + " initialized!");
+}
+
+db.close(function() {
+	console.log("Database Closed");
+});
