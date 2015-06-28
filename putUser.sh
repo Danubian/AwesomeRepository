faa4f723d999da7341d6d8e3c21813bf88ae50f6
Zack Maldonado <stjohnzack@yahoo.com>
2015-06-28 03:10:05 -0400

    Putting ship

--- putUser.sh
+++ putUser.sh
@@ -1,1 +1,1 @@
-curl -d '{ "id" : "danubian", "red" : "1f", "green" : "0f", "blue" : "0f", "alpha" : "1f" }' -H "Content-Type: application/json" -X PUT http://54.173.208.168:80/user
+curl -d '{ "id" : "danubian", "red" : "1f", "green" : "0f", "blue" : "0f", "alpha" : "1f" }' -H "Content-Type: application/json" -X PUT http://54.173.208.168:80/ship
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var server = app.listen(port, function () {
   console.log('Example app listening at http://%s:%s', host, port);
 });
 
-//Hanlde JSON
+//Handle JSON
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
@@ -27,7 +27,19 @@ app.get('/', function (req, res) {
 //})
 
 //PUT ip.address.whatever:80/user~~somepayload json
-app.put('/user', function (req, res) {
+app.put('/ship', function (req, res) {
+	//Creates an SQL command/statement
+	var stmt = db.prepare("INSERT OR REPLACE INTO users (id,username) VALUES (?, ?, ?, ?, ?)");
+	stmt.run(req.body.id, req.body.red, req.body.green, req.body.blue, red.body.alpha);
+	stmt.finalize();
+
+	console.log("Added Ship");
+	
+	//Test code, displays all users in the console
+	db.each("SELECT id, red, green, blue, alpha FROM ships", function(err, row) {
+	  console.log(row.id + ": " + row.red + ", " + row.green + ", " + row.blue + ", " + row.alpha);
+	});
+
 	res.send(req.body);
 })
 
